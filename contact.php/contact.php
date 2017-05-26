7f7796bdc4c028acb6d8dced7e078b77fa0da86d
qenqoro13 <castillasanchez@yahoo.com>
2017-05-25 22:25:48 -0500

    CAMBIOS  PHP  CONTACTO ICOMOON
    CAMBIOS  EN  HTML5  PHP  ICOMOON

--- contact.php/contact.php
+++ contact.php/contact.php
@@ -3,17 +3,17 @@
 $ para= 'castillasanchez@yahoo.com';
 $ asunto = "Mensaje desde jotajotavm.com";
 
-$ mailheader = "Form:"$_POST ["email"]."\r\n";
-$ mailheader = "Reply-To:"$_POST ["email"]."\r\n";
-$ mailheader = "Content-type: text/html; charset-utf_8\r\n"
+$ mailheader = "From:".$_POST ["email"]."\r\n";
+$ mailheader.= "Reply-To:".$_POST ["email"]."\r\n";
+$ mailheader.= "Content-type: text/html; charset-utf=8\r\n"
 
-$MESSAGE_BODY= "Nombre: "$_POST ["name"]."\n";
+$MESSAGE_BODY= "Nombre: ".$_POST ["name"]."\n";
 $MESSAGE_BODY.= "\n<br>Email:".$_POST["email"]."\n";
-$MESSAGE_BODY.="\n<br>Mensaje:"n12br($_POST["message"])."\n";
+$MESSAGE_BODY.="\n<br>Mensaje:".n12br($_POST["message"])."\n";
 
 mail($para, $asunto, $MESSAGE_BODY, $mailheader) or die("Error
 	al enviar email!");
-header('Location:https//qenqoro13.github.io/proyecto/')
+header('Location: http://jotajotavm.com/sonia')
 
 ?>
 
