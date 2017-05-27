f4ff326aa5dcbd82ddb47e5727646f584c9cd10c
qenqoro13 <castillasanchez@yahoo.com>
2017-05-26 23:16:44 -0500

    dasdasd
    qwdqwd

--- contact.php/contact.php
+++ contact.php/contact.php
@@ -1,11 +1,11 @@
 
-<? php
-$ para= 'castillasanchez@yahoo.com';
-$ asunto = "Mensaje desde jotajotavm.com";
+<?php
+$para= 'castillasanchez@yahoo.com';
+$asunto = "Mensaje desde jotajotavm.com";
 
-$ mailheader = "From:".$_POST ["email"]."\r\n";
-$ mailheader.= "Reply-To:".$_POST ["email"]."\r\n";
-$ mailheader.= "Content-type: text/html; charset-utf=8\r\n"
+$mailheader = "From:".$_POST ["email"]."\r\n";
+$mailheader.= "Reply-To:".$_POST ["email"]."\r\n";
+$mailheader.= "Content-type: text/html; charset-utf=8\r\n"
 
 $MESSAGE_BODY= "Nombre: ".$_POST ["name"]."\n";
 $MESSAGE_BODY.= "\n<br>Email:".$_POST["email"]."\n";
